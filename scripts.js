0cb9f2c720622f6461e19ea3e13f762896eb281d
jwcomplete <68453307+jwcomplete@users.noreply.github.com>
2025-03-21 01:32:00 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,11 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("Website Loaded!");
-
-    // Button Click Events
-    let buttons = document.querySelectorAll("button");
-    buttons.forEach(button => {
-        button.addEventListener("click", function () {
-            alert("Feature coming soon!");
-        });
-    });
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Website Loaded!');
 });
