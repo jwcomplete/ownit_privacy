6627cb31f688267a9526aea7523ac65b1ee82867
jwcomplete <68453307+jwcomplete@users.noreply.github.com>
2025-03-21 01:50:04 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Website Loaded!');
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Website Loaded!");
+
+    let buttons = document.querySelectorAll("button");
+    buttons.forEach(button => {
+        button.addEventListener("click", function () {
+            alert("Feature coming soon!");
+        });
+    });
 });
